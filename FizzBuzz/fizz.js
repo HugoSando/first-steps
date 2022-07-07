a5ed2ede4f5b660d2f38114d1e7e6c061382917d
HugoSando <hugosa291@gmail.com>
2022-07-07 15:05:06 -0600

    Proyecto FizzBuzz

---
+++ FizzBuzz/fizz.js
@@ -0,0 +1,24 @@
+var numeros = 100;
+var fizz = "";
+
+for(var i=1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        document.write("FizzBuzz");
+    }
+    else if(i % 3 == 0)
+    {
+        document.write("Fizz");
+    }
+    else if(i % 5 == 0)
+    {
+        document.write("Buzz");
+    }
+    else
+    {
+        document.write(i);
+    }
+
+    document.write("<br />");
+}
